7b42cc195c9bc2084f8f0dcd4f91d75fc6ea216d
Gustavo Siqueira <gus@brid-it.com>
2021-09-21 12:04:27 -0300

    feat(redis): stores implementation

--- src/Redis.php
+++ src/Redis.php
@@ -12,12 +12,26 @@ class Redis
 
   public function __construct()
   {
+    $this->store();
+  }
+
+  public function store(string $name = null)
+  {
     $cacheConfig = config('cache');
 
-    $settings = $cacheConfig['redis']['default'] ?? [];
+    $name ??= $cacheConfig['default'];
+    $connection = $cacheConfig['stores'][$name]['connection'] ?? null;
+
+    if ($connection === null) {
+      return;
+    }
+
+    $settings = $cacheConfig['redis'][$connection] ?? [];
     $options = $cacheConfig['redis']['options'] ?? [];
 
     $this->client = new Client($settings, $options);
+
+    return $this;
   }
 
   public function __call(string $name , array $arguments): mixed
