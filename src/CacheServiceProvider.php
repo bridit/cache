5b1d35af09f1464f437a0b655a1762eb50fba82d
Gustavo Siqueira <gus@brid-it.com>
2021-09-22 15:01:22 -0300

    feat: laravel cache

--- src/CacheServiceProvider.php
+++ src/CacheServiceProvider.php
@@ -3,13 +3,41 @@
 namespace Brid\Cache;
 
 use Brid\Core\Foundation\Providers\ServiceProvider;
+use Illuminate\Cache\CacheManager;
+use Illuminate\Cache\MemcachedConnector;
+use Illuminate\Cache\RateLimiter;
+use Illuminate\Redis\RedisManager;
 
 class CacheServiceProvider extends ServiceProvider
 {
 
   public function boot()
   {
-    $this->container->set('cache', new Redis());
+    $this->container->set('redis', function ($app) {
+      return new RedisManager($app, $app['config']['database.redis.client'], $app['config']['database.redis']);
+    });
+
+    $this->container->set('cache', function ($app) {
+      return new CacheManager($app);
+    });
+
+    $this->container->set('cache.store', function ($app) {
+      return $app['cache']->driver();
+    });
+
+    $this->container->set('cache.psr6', function ($app) {
+      return new Psr16Adapter($app['cache.store']);
+    });
+
+    $this->container->set('memcached.connector', function () {
+      return new MemcachedConnector;
+    });
+
+    $this->container->set(RateLimiter::class, function ($app) {
+      return new RateLimiter($app->make('cache')->driver(
+        $app['config']->get('cache.limiter')
+      ));
+    });
   }
 
 }
